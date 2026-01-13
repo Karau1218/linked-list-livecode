b708dc9eb615dbf6caff477a4f9a167527435097
Fkarau <30944677+Karau1218@users.noreply.github.com>
2026-01-13 14:50:05 -0800

    done w the file

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,30 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("~linked lists practice~");
+        // System.out.println("~linked lists practice~");
+        Node head = new Node('g', null);
+        // System.out.println(head.value);
+        // System.out.println(head.next);
+
+        Node ryan = new Node('s', null);
+        head.next = ryan;
+        ryan.next = new Node('t', null);
+        ryan.next.next = new Node('x', null);
+
+        head =  head.next;
+        printList(head);
+
+    }   
+    public static void printList(Node start) {
+        Node current = start;
+
+        while(current != null) {
+            System.out.println(current.value);
+            current = current.next;
+        }
+        
     }
-}
+
+        // System.out.println(head.next.next.value);
+    } 
+
+ 
