66bfe0db86f8f18164c52fef274e988031bef58d
Fkarau <30944677+Karau1218@users.noreply.github.com>
2026-01-15 14:53:12 -0800

    added myll code

--- src/Practice.java
+++ src/Practice.java
@@ -10,8 +10,14 @@ public class Practice {
         ryan.next = new Node('t', null);
         ryan.next.next = new Node('x', null);
 
-        head =  head.next;
-        printList(head);
+        // head =  head.next;
+        // printList(head);
+        // Prints out true
+        System.out.println(contains(head, 'x'));
+        // Prints out false
+        System.out.println(contains(head, 'e'));
+
+            remove(head, 't'); 
 
     }   
     public static void printList(Node start) {
@@ -25,6 +31,31 @@ public class Practice {
     }
 
         // System.out.println(head.next.next.value);
+    public static boolean contains(Node start, char toFind) {
+        Node head = start;
+        while (head != null) {
+            if (head.value == toFind) {
+                return true;
+            }
+
+            head = head.next;
+        }
+
+        return false; 
+    }
+    // Removes the first node that has toRemove
+    public static char remove(Node head, char toRemove) {
+        Node current = head;
+
+        while (current.next != null) {
+            if (current.value == toRemove) {
+                current.next = current.next.next;
+                return toRemove;
+            }
+            current = current.next;
+        }
+            return '\0';
+    }
     } 
 
  
